5768a92f4950df4723ef530c5d7f859e485243f2
SHI YI <shiyi2008@gmail.com>
2019-03-27 12:44:51 -0400

    fixed comment position error, add another printwriter that output pure token files.

--- src/main/java/edu/nyu/compiler/scanner/LineInput.java
+++ src/main/java/edu/nyu/compiler/scanner/LineInput.java
@@ -1,6 +1,7 @@
 package edu.nyu.compiler.scanner;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 class LineInput {
 
@@ -10,24 +11,27 @@ class LineInput {
     private boolean flagComment;
     private final String currLine;
 
+    private PrintWriter outStreamForPar;
     private PrintWriter outStream;
     private int currPos = 0;
     private int startPos = 0;
     private TokenContext currContext = null; // This is the context switch
+    ArrayList<String> tokenStorage;
 
-    public LineInput(String currLine, int lineNumber, PrintWriter outStream) {
+    public LineInput(String currLine, int lineNumber, PrintWriter outStream, PrintWriter outStreamForPar) {
         this.currLine = currLine;
         this.lineNumber = lineNumber;
         this.outStream = outStream;
-
+        this.outStreamForPar = outStreamForPar;
+        this.tokenStorage= new ArrayList<String>();
         System.out.println("~~~~~~>Line content<~~~~~~");
         System.out.println(currLine);
         System.out.println("~~~~~~^Line content^~~~~~~");
     }
 
     public Token next() {
-        while(!endOfLine()) {
 
+        while(!endOfLine()) {
             char currChar = currLine.charAt(currPos++);
 
             currContext = getContextType(currChar);
@@ -41,6 +45,7 @@ class LineInput {
                 outStream.printf("unidentified character %c at line: %d, col: %d%n%n",currChar,lineNumber,currPos);
                 System.out.printf("unidentified character %c at line: %d, col: %d%n%n",currChar,lineNumber,currPos);
                 continue;
+
             } else if (currContext == TokenContext.NonGreedyOperator) {
                 NonGreedyOperator possibleToken = new NonGreedyOperator(String.valueOf(currChar),lineNumber,startPos,currPos);
                 //already checked
@@ -56,8 +61,8 @@ class LineInput {
                             && currLine.charAt(currPos+1)=='*') {
                         //comment encountered, break loop.
                         String comment = currLine.substring(currPos-1,currLine.length()) ;
-                        outStream.println(String.format("comment: \"%s\" from char %d to %d on line: %d\n",comment,startPos,currPos,lineNumber));
-                        System.out.println(String.format("comment: \"%s\" from char %d to %d on line: %d\n",comment,startPos,currPos,lineNumber));
+                        outStream.println(String.format("comment: \"%s\" from char %d to %d on line: %d\n",comment,startPos,currLine.length(),lineNumber));
+                        System.out.println(String.format("comment: \"%s\" from char %d to %d on line: %d\n",comment,startPos,currLine.length(),lineNumber));
                         this.flagComment = true;
                         break;
 
@@ -212,10 +217,11 @@ class LineInput {
                 }
             }
         }
+
         startPos = currPos;
         printTokenInfo(currToken);
+        addTokenToStorage(currToken);
         return currToken;
-
     }
 
     public Token peek() {
@@ -244,11 +250,28 @@ class LineInput {
         return context;
     }
 
+    public void printTokenForParser(){
+        if (this.tokenStorage.size()>0) {
+            String outLinePar = this.tokenStorage.get(0);
+            for (int i = 1; i < this.tokenStorage.size(); i++) {
+                outLinePar += ' ';
+                outLinePar += this.tokenStorage.get(i);
+            }
+            outStreamForPar.println(outLinePar);
+            System.out.println(outLinePar);
+        }
+    }
+
+    public void addTokenToStorage(Token token){
+        if (token!=null){ this.tokenStorage.add(token.getKwType().name());}
+    }
     public void printTokenInfo(Token token) {
         if (token != null){
-            System.out.println(token.getKwType());
+            String tokenName = token.getKwType().name();
+            System.out.println(tokenName);
             System.out.println(String.valueOf(token.getStartCharPos())+' '+ String.valueOf(token.getFinishCharPos()));
-            outStream.println(String.format("%s on line %d, from char %d to %d",token.getKwType().name(),lineNumber,token.getStartCharPos(),token.getFinishCharPos()));
+            outStream.println(String.format("%s on line %d, from char %d to %d",tokenName,lineNumber,token.getStartCharPos(),token.getFinishCharPos()));
+
         }
         System.out.println("\n");
     }
--- src/main/java/edu/nyu/compiler/scanner/Number.java
+++ src/main/java/edu/nyu/compiler/scanner/Number.java
@@ -14,4 +14,5 @@ public class Number extends Token {
             return false; 
         }
     }
+
 }
