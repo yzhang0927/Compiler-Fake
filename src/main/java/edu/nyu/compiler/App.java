19d942b4db7ec8af08ca7dfe01f401b2e3277d88
Yu Zhang <zhangyu@nyu.edu>
2019-04-18 14:02:12 -0400

    feature#1 id int replacement
    need to figure out line input

--- src/main/java/edu/nyu/compiler/App.java
+++ src/main/java/edu/nyu/compiler/App.java
@@ -2,12 +2,15 @@ package edu.nyu.compiler;
 
 import edu.nyu.compiler.parser.CalcLexer;
 import edu.nyu.compiler.parser.CalcParser;
+import edu.nyu.compiler.scanner.idint.IdIntReplace;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -17,6 +20,12 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
+        List<String> filePaths = new ArrayList<String>(){{
+            add("./test/IntId");
+        }};
+
+        IdIntReplace.replaceFile(filePaths);
+
         System.out.println( "Hello World!" );
         CharStream charStream = CharStreams.fromFileName("./test/sample.txt");
         CalcLexer calcLexer = new CalcLexer(charStream);
--- src/main/java/edu/nyu/compiler/scanner/idint/IdIntReplace.java
+++ src/main/java/edu/nyu/compiler/scanner/idint/IdIntReplace.java
@@ -1,15 +1,51 @@
 package edu.nyu.compiler.scanner.idint;
 
-import java.io.InputStream;
+import java.io.*;
+import java.util.List;
 import java.util.Scanner;
 
 public class IdIntReplace {
-    public void replace(InputStream stream) {
-        Scanner scanner = new Scanner(stream);
-        while (scanner.hasNext()) {
-            if (scanner.next().length() > 80) {
 
+    private static final String TOO_LONG_ID_REPLACEMENT = "$ID$TOO$LONG";
+    private static final String OUT_OF_BOUND_INT_REPLACEMENT = "-1";
+    private static final String NUM_REGEX = "\\d+";
+    private static final String TXT = ".txt";
+    private static final String INT_ID_OUT = ".iio";
+
+    public static void replaceFile(List<String> filePaths) throws IOException {
+        for (String filePath : filePaths) {
+            File file = new File(filePath + TXT);
+            InputStream inputStream = new FileInputStream(file);
+            PrintStream printStream = new PrintStream(filePath + INT_ID_OUT);
+            replace(inputStream, printStream);
+            printStream.flush();
+            printStream.close();
+            inputStream.close();
+        }
+    }
+
+    /**
+     * replace the invalid ID and number from the inputstream
+     * and output the replaced stream to the printstream
+     * @param inputStream
+     * @param printStream
+     */
+    public static void replace(InputStream inputStream, PrintStream printStream) {
+        Scanner scanner = new Scanner(inputStream);
+        int countTooLongID = 0;
+        String currOutputString;
+        while (scanner.hasNext()) {
+            currOutputString = scanner.next();
+            if (currOutputString.length() > 80) {
+                currOutputString = TOO_LONG_ID_REPLACEMENT + countTooLongID++;
+            } else if (currOutputString.matches(NUM_REGEX)) {
+                try {
+                    Integer.parseInt(currOutputString);
+                } catch (NumberFormatException nfe) {
+                    currOutputString = OUT_OF_BOUND_INT_REPLACEMENT;
+                }
             }
+            printStream.append(currOutputString + " ");
         }
     }
 }
